5b8c19c11bc30ebfe1bb4923308043eba5f4826c
Iyinoluwa Tugbobo <iyinoluwatugbobo@big-mom>
2021-01-24 23:17:14 -0500

    Added Hello World

--- large-scale-programming/src/org/howard/edu/lsp/assignment1/helloworld.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment1/helloworld.java
@@ -3,6 +3,7 @@ package org.howard.edu.lsp.assignment1;
 public class helloworld {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World!!");
 		// Iyinoluwa (Ian) Tugbobo
 	}
 
