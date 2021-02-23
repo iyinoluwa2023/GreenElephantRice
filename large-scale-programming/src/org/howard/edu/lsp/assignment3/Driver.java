b98cd5910bc0bb21d5637eeadf034e1d55db89a7
iyinoluwatugbobo <iyinoluwa2023@gmail.com>
2021-02-22 20:41:26 -0500

    Clear and length methods added

--- large-scale-programming/src/org/howard/edu/lsp/assignment3/Driver.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment3/Driver.java
@@ -1,4 +1,35 @@
 package org.howard.edu.lsp.assignment3;
 
 public class Driver {
+
+
+    public static void print_test(String test_string){
+        System.out.println("------------------------------------------------" + "\n" + test_string + "\n");
+    }
+
+    public static void test_clear(IntegerSet test_set){
+        // clear test
+        test_set.add(5);
+        print_test("Clear Method");
+        System.out.println("Before: \n" + test_set.toString());
+        test_set.clear();
+        System.out.println("After: \n" + test_set.toString());
+    }
+
+    public static void test_length(IntegerSet test_set, int n) {
+        for (int i = 0; i < n; i++) {
+            test_set.add(i);
+        }
+        print_test("Length Method");
+        System.out.println("Expected: " + n);
+        System.out.println(test_set.toString() + "\n");
+        System.out.println("test_set.length(): " + test_set.length());
+    }
+
+    public static void main(String[] args) {
+        IntegerSet test_set = new IntegerSet();
+
+        test_clear(test_set);
+        test_length(test_set, 5);
+    }
 }
--- large-scale-programming/src/org/howard/edu/lsp/assignment3/IntegerSet.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment3/IntegerSet.java
@@ -1,4 +1,54 @@
 package org.howard.edu.lsp.assignment3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntegerSet {
+    private List<Integer> set = new ArrayList<Integer>();
+
+    public void clear() {
+        set = new ArrayList<Integer>();
+    }
+    public int length() {
+        return set.size();
+    }
+    public boolean equals(IntegerSet b) {
+        return false;
+    }
+    // Returns true if the set contains the value, otherwise false
+    public boolean contains(int value) {
+        return false;
+    }
+
+//    // Returns the largest item in the set; Throws a IntegerSetException if the set is empty
+//    public int largest() throws IntegerSetException {…};
+//
+//    // Returns the smallest item in the set; Throws a IntegerSetException if the set is empty
+//    public int smallest() throws IntegerSetException;
+//
+    // Adds an item to the set or does nothing it already there
+    public void add(int item) {
+        set.add(item);
+    }; // adds item to s or does nothing if it is in set
+//
+//    // Removes an item from the set or does nothing if not there
+//    public void remove(int item) {…};
+//
+//    // Set union
+//    public static void union(IntegerSet intSetb) {…};
+//
+//    // Set intersection
+//    public static public intSet intersect(IntegerSet intSetb) {…};
+//
+//    // Set difference, i.e., s1 –s2
+//    public static intSet diff(IntegerSet intSetb); // set difference, i.e. s1 - s2
+//
+//    // Returns true if the set is empty, false otherwise
+//    boolean isEmpty();
+//
+    // Return String representation of your set
+    public String toString() {
+        return set.toString();
+    };	// return String representation of your set
+
 }
