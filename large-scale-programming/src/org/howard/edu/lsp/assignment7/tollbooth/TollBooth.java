3947134330a7b4f1293d7fcf51962042eb1fd718
iyinoluwa2023 <73270351+iyinoluwa2023@users.noreply.github.com>
2021-04-19 02:29:12 -0400

    Javadoc comments added to the Truck abstract class and TollBooth interface

--- large-scale-programming/src/org/howard/edu/lsp/assignment7/tollbooth/TollBooth.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment7/tollbooth/TollBooth.java
@@ -1,10 +1,39 @@
 package org.howard.edu.lsp.assignment7.tollbooth;
 
 public interface TollBooth {
-    public void logTruck(Truck t);
-    int currentTruckCount();
-    public int calculateToll(Truck t);
+    /**
+     * Helper function to add a Truck to a tollbooth's working array of Trucks.
+     * @param truck any implementation of a truck object
+     */
+    public void logTruck(Truck truck);
+
+    /**
+     * Helper function to return the size of the tollbooth's working array of trucks.
+     * @return the size of the array
+     */
+    public int currentTruckCount();
+
+    /**
+     * Calculates the toll of a truck and adds the truck to the tollbooth's working array of trucks.
+     * @param truck any implementation of a truck object
+     * @return the cost of the toll for that truck
+     */
+    public int calculateToll(Truck truck);
+
+    /**
+     * Iterates through the tollbooth's working array of trucks and calculates the total tolls paid.
+     * @return the total tolls for each truck that has used the tollbooth since last collection
+     */
     public int calculateTotal();
+
+    /**
+     * Displays the data of a tollbooth at a certain point.
+     * @return the string of tbe data printout for the tollbooth
+     */
     public String displayData();
+
+    /**
+     * Resets the tollbooth. Essentially "collects" from the tollbooth.
+     */
     public void reset();
 }
--- large-scale-programming/src/org/howard/edu/lsp/assignment7/tollbooth/Truck.java
+++ large-scale-programming/src/org/howard/edu/lsp/assignment7/tollbooth/Truck.java
@@ -4,9 +4,17 @@ public abstract class Truck {
     protected int axles = 0;
     protected int weight = 0;
 
+    /**
+     * Get function for the weight of the truck object
+     * @return the weight of the truck object
+     */
     public int getWeight() {
         return this.weight;
     }
+    /**
+     * Get function for the axles of the truck object
+     * @return the axle count of the truck object
+     */
     public int getAxles() {
         return this.axles;
     }
